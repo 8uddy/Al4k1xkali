1c2b264ba6a7f948ef8e44d799c348fbb1aff46a
buddy <buddy@buddys-Air.localdomain>
2023-06-16 00:10:44 +0200

    create simple nmap script and save results

--- kali/kali-files/.bash_history
+++
@@ -1,7 +0,0 @@
-pwd
-ls
-nano nmap.sh
-sh nmap.sh 
-ls nmap_scan_results.txt 
-cat nmap_scan_results.txt 
-exit
--- kali/kali-files/nmap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGET="172.19.0.2" # Ersetzen Sie dies durch die IP-Adresse Ihres Zielhosts
-OUTPUT_FILE="nmap_scan_results.txt"
-
-echo "Starte Scan des Ziels $TARGET"
-nmap -v -A $TARGET > $OUTPUT_FILE
-
-echo "Scan abgeschlossen. Ergebnisse gespeichert in $OUTPUT_FILE"
